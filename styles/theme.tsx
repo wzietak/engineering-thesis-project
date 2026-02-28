86956bd5e2af4ba84c6f492a06b03dd79421029b
Wiktoria Zietak <maullido.blog@gmail.com>
2026-02-28 10:20:52 +0100

    Change of value for BorderRadius, addition of primary-light color

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -8,7 +8,8 @@ export const theme = {
         green: '#D7F5CA',
         pink: '#F5CAF4',
         lightblue: '#CAF5F3',
-        grey: '#CFD7DA'
+        grey: '#CFD7DA',
+        primary_light: '#393939',
     },
     deckColors: [
         '#B8CBFA',
@@ -19,7 +20,7 @@ export const theme = {
     borderRadius: {
         sm: 12,
         md: 18,
-        lg: 24,
+        lg: 32,
     },
     boxShadow: {
         buttons: '0px 0px 18px rgba(0, 0, 0, 0.25)'
