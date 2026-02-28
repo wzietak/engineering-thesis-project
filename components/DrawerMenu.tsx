bca742a75947db0c29e48cddd11b982378cade6c
Wiktoria Zietak <maullido.blog@gmail.com>
2026-02-28 08:45:36 +0100

    Added icons, labels style and tintColor

--- components/DrawerMenu.tsx
+++ components/DrawerMenu.tsx
@@ -1,3 +1,4 @@
+import { theme } from '@/styles/theme';
 import Octicons from '@expo/vector-icons/Octicons';
 import {
 	DrawerContentComponentProps,
@@ -5,16 +6,58 @@ import {
 	DrawerItem,
 } from '@react-navigation/drawer';
 import { router } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
 
 export default function DrawerMenu(props: DrawerContentComponentProps) {
 	return (
 		<DrawerContentScrollView {...props}>
+			<View style={styles.emptyView}></View>
 			<DrawerItem
 				label={'Decks'}
 				onPress={() => router.push('/')}
-				icon={({ size, color }) => (
-					<Octicons name='rows' size={size} color={color} />
-				)}></DrawerItem>
+				icon={({ size }) => (
+					<Octicons name='rows' size={size} color={theme.colors.primary} />
+				)}
+				labelStyle={styles.labelText}
+				focused={
+					props.state.routes[props.state.index].name == 'index' ? true : false
+				}
+				activeTintColor={theme.colors.blue}
+				inactiveTintColor={theme.colors.primary}></DrawerItem>
+			<DrawerItem
+				label={'Browse cards'}
+				onPress={() => router.push('/browse-cards')}
+				icon={({ size }) => (
+					<Octicons name='versions' size={size} color={theme.colors.primary} />
+				)}
+				labelStyle={styles.labelText} focused={
+					props.state.routes[props.state.index].name == 'browse-cards' ? true : false
+				}
+				activeTintColor={theme.colors.blue}
+				inactiveTintColor={theme.colors.primary}></DrawerItem>
+			<DrawerItem
+				label={'Settings'}
+				onPress={() => router.push('/general-settings')}
+				icon={({ size }) => (
+					<Octicons name='gear' size={size} color={theme.colors.primary} />
+				)}
+				labelStyle={styles.labelText} focused={
+					props.state.routes[props.state.index].name == 'general-settings' ? true : false
+				}
+				activeTintColor={theme.colors.blue}
+				inactiveTintColor={theme.colors.primary}></DrawerItem>
 		</DrawerContentScrollView>
 	);
 }
+
+const styles = StyleSheet.create({
+	emptyView: {
+		height: 120,
+		width: '100%',
+	},
+	labelText: {
+		fontSize: theme.fontSize.md,
+		color: theme.colors.primary,
+		fontWeight: 'bold',
+	},
+});
