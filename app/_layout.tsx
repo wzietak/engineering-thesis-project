a61c0eec43330262fd0e5e20c128498db20b0e56
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-08 16:59:01 +0100

    Added Stack Screen for 'add new card' view and set showing customized AppHeader for it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
-import { Stack } from 'expo-router';
+import AppHeader from '@/components/AppHeader';
+import { DrawerActions } from '@react-navigation/native';
+import { Stack, useRouter } from 'expo-router';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 export default function RootLayout() {
 	const [optionsVisible, setOptionsVisible] = useState(false);
+	const router = useRouter();
 	return (
 		<View style={styles.mainContainer}>
 			<Stack screenOptions={{ headerShown: false }}>
 				<Stack.Screen name='(drawer)' />
+				<Stack.Screen name='add-new-card' options={{ title: 'Add new card', presentation: 'modal', headerShown: true, header: (props) => {
+										return (
+											<AppHeader
+												title={String(props.options.title)}
+												showBack={true} showOptions={false}
+												goBack={()=>{router.back()}}
+												></AppHeader>
+										);
+									}}}></Stack.Screen>
 			</Stack>
 		</View>
 	);
