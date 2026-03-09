129f92145453540fe663eed4f7c638badc5cd991
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-09 21:38:15 +0100

    Added ScrollView, changed style for 'Example of use' textInput; added container for bottom button 'Save' so Scroll can go underneath it and the whole form will look better

--- app/add-new-card.tsx
+++ app/add-new-card.tsx
@@ -1,7 +1,14 @@
 import { theme } from '@/styles/theme';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import { useState } from 'react';
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+	Pressable,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
@@ -17,86 +24,103 @@ export default function AddNewCard() {
 	]);
 
 	return (
-		<View style={styles.mainContainer}>
-			<Text style={[styles.formText, { paddingTop: 0 }]}>Deck</Text>
-			<DropDownPicker
-				open={open}
-				value={value}
-				items={items}
-				setOpen={setOpen}
-				setValue={setValue}
-				setItems={setItems}
-				disabled={true}
-				style={styles.dropdown}
-			/>
-			<Text style={styles.formText}>Card type</Text>
-			<DropDownPicker
-				open={open}
-				value={value}
-				items={items}
-				setOpen={setOpen}
-				setValue={setValue}
-				setItems={setItems}
-				style={styles.dropdown}
-			/>
-
-			<Text style={styles.formText}>Front</Text>
-			<TextInput style={styles.textInput} />
-			<Text style={styles.formText}>Back</Text>
-			<TextInput style={styles.textInput} />
-			<Text style={styles.formText}>Example of use</Text>
-			<TextInput
-				style={[styles.textInput, { borderColor: theme.colors.purple }]}
-			/>
-			<Pressable style={styles.genwithAIContent}>
-				<SimpleLineIcons
-					name='magic-wand'
-					size={24}
-					color={theme.colors.purple}
-					style={{
-						textShadowRadius: 30,
-						textShadowColor: theme.colors.purple_alpha,
-					}}
+		<View style={[styles.container, { paddingBottom: insets.bottom }]}>
+			<ScrollView
+				style={styles.scrollContainer}
+				showsVerticalScrollIndicator={false}>
+				<Text style={[styles.formText, { paddingTop: 0 }]}>Deck</Text>
+				<DropDownPicker
+					open={open}
+					value={value}
+					items={items}
+					setOpen={setOpen}
+					setValue={setValue}
+					setItems={setItems}
+					disabled={true}
+					style={styles.dropdown}
 				/>
-				<Text
+				<Text style={styles.formText}>Card type</Text>
+				<DropDownPicker
+					open={open}
+					value={value}
+					items={items}
+					setOpen={setOpen}
+					setValue={setValue}
+					setItems={setItems}
+					style={styles.dropdown}
+				/>
+
+				<Text style={styles.formText}>Front</Text>
+				<TextInput style={styles.textInput} />
+				<Text style={styles.formText}>Back</Text>
+				<TextInput style={styles.textInput} />
+				<Text style={styles.formText}>Example of use</Text>
+				<TextInput
 					style={[
-						styles.formText,
+						styles.textInput,
 						{
-							paddingHorizontal: 10,
-							color: theme.colors.purple,
+							borderColor: theme.colors.purple,
+							height: 80,
+							textAlignVertical: 'top',
+						},
+					]}
+					multiline={true}
+				/>
+				<Pressable style={styles.genwithAIContent}>
+					<SimpleLineIcons
+						name='magic-wand'
+						size={24}
+						color={theme.colors.purple}
+						style={{
 							textShadowRadius: 30,
 							textShadowColor: theme.colors.purple_alpha,
-						},
-					]}>
-					{' Generate with AI '}
-				</Text>
-			</Pressable>
-			<Text style={[styles.formText, { paddingTop: 0 }]}>Tags</Text>
-			<DropDownPicker
-				open={open}
-				value={value}
-				items={items}
-				setOpen={setOpen}
-				setValue={setValue}
-				setItems={setItems}
-				multiple={true}
-				style={styles.dropdown}
-			/>
-			<Pressable style={styles.buttonContainer}>
-				<Text>Save</Text>
-			</Pressable>
+						}}
+					/>
+					<Text
+						style={[
+							styles.formText,
+							{
+								paddingHorizontal: 10,
+								color: theme.colors.purple,
+								textShadowRadius: 30,
+								textShadowColor: theme.colors.purple_alpha,
+							},
+						]}>
+						{' Generate with AI '}
+					</Text>
+				</Pressable>
+				<Text style={[styles.formText, { paddingTop: 0}]}>Tags</Text>
+				<DropDownPicker
+					open={open}
+					value={value}
+					items={items}
+					setOpen={setOpen}
+					setValue={setValue}
+					setItems={setItems}
+					multiple={true}
+					style={[styles.dropdown, { marginBottom: 30}]}
+				/>
+			</ScrollView>
+			<View style= {styles.buttonContainer}>
+				<Pressable
+					style={[styles.buttonPressable]}>
+					<Text style={styles.buttonText}>Save</Text>
+				</Pressable>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	mainContainer: {
-		paddingBottom: 10,
-		paddingHorizontal: 20,
+	container: {
+		width: '100%',
 		flex: 1,
 		flexDirection: 'column',
+		alignItems: 'center',
 		backgroundColor: theme.colors.background,
-		justifyContent: 'flex-start',
+	},
+	scrollContainer: {
+		paddingHorizontal: 20,
 	},
 	formText: {
 		paddingTop: 10,
@@ -110,6 +134,7 @@ const styles = StyleSheet.create({
 		borderColor: theme.colors.primary,
 	},
 	textInput: {
+		paddingHorizontal: 10,
 		minHeight: 45,
 		borderWidth: 1,
 		borderRadius: theme.borderRadius.sm,
@@ -126,12 +151,26 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	buttonContainer: {
-		paddingTop: 30,
-		height: 48,
-		borderRadius: theme.borderRadius.md,
+		height: 100,
+		width: '100%',
+		paddingHorizontal: 20,
+		paddingTop: 10,
+		backgroundColor: theme.colors.background,
+		boxShadow: theme.boxShadow.bottomContainer,
+	},
+	buttonPressable: {
+		maxHeight: 60,
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: theme.borderRadius.lg,
 		boxShadow: theme.boxShadow.buttons,
 		backgroundColor: theme.colors.primary,
+	},
+	buttonText: {
+		alignSelf: 'center',
 		color: theme.colors.background,
-
-	}
+		fontFamily: theme.fontFamily.bold,
+		fontSize: theme.fontSize.lg,
+	},
 });
