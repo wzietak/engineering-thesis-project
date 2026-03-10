c0d8f43cebd33ab43038dfd275f257de4dbdcdbd
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-10 07:09:32 +0100

    Moved custom AppHeader to separate variable so it can be reused without repeating. Added Stack.Screen for 'add-new-deck' screen.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import AppHeader from '@/components/AppHeader';
-import { DrawerActions } from '@react-navigation/native';
 import { Stack, useRouter } from 'expo-router';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
@@ -7,19 +6,38 @@ import { StyleSheet, View } from 'react-native';
 export default function RootLayout() {
 	const [optionsVisible, setOptionsVisible] = useState(false);
 	const router = useRouter();
+	const headerStyle = {
+		header: (props: any) => (
+			<AppHeader
+				title={String(props.options.title)}
+				showBack={true}
+				showOptions={false}
+				goBack={() => {
+					router.back();
+				}}></AppHeader>
+		),
+	};
+
 	return (
 		<View style={styles.mainContainer}>
 			<Stack screenOptions={{ headerShown: false }}>
 				<Stack.Screen name='(drawer)' />
-				<Stack.Screen name='add-new-card' options={{ title: 'Add new card', presentation: 'modal', headerShown: true, header: (props) => {
-										return (
-											<AppHeader
-												title={String(props.options.title)}
-												showBack={true} showOptions={false}
-												goBack={()=>{router.back()}}
-												></AppHeader>
-										);
-									}}}></Stack.Screen>
+				<Stack.Screen
+					name='add-new-card'
+					options={{
+						...headerStyle,
+						title: 'Add new card',
+						presentation: 'modal',
+						headerShown: true,
+					}}></Stack.Screen>
+				<Stack.Screen
+					name='add-new-deck'
+					options={{
+						...headerStyle,
+						title: 'Create new deck',
+						presentation: 'modal',
+						headerShown: true,
+					}}></Stack.Screen>
 			</Stack>
 		</View>
 	);
