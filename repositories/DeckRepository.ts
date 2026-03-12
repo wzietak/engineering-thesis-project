12a53a5edb0368fbef6b3823897929c652981057
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-12 07:52:02 +0100

    Created Card and Deck repositories with interfaces containing main CRUD functions

---
+++ repositories/DeckRepository.ts
@@ -0,0 +1,9 @@
+import { Deck } from "@/models/deck";
+
+
+export interface DeckRepository {
+    createNewDeck: (deckData : Omit<Deck, 'id'>) => Promise<Deck>;
+    getDecks: () => Promise<Deck[]>;
+    updateDeck: (deckData: Deck) => Promise<Deck>;
+    deleteDeck: (deckId: number) => Promise<boolean>;
+}
