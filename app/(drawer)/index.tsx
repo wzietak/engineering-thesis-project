fef702087f210a0601724136dd8b3b7e164d104a
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-13 11:21:08 +0100

    Replaced ScrollView with FlatList and dynamically generated decks

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,3 +1,4 @@
+import DeckComponent from "@/components/Deck";
 import FloatingButton from "@/components/FloatingButton";
 import Overlay from "@/components/Overlay";
 import { decksExampleData } from "@/data/MockData";
@@ -6,7 +7,7 @@ import { MockDeckRepository } from "@/repositories/MockDeckRepository";
 import { theme } from "@/styles/theme";
 import { useFocusEffect, useRouter } from "expo-router";
 import { useCallback, useEffect, useState } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const decks = decksExampleData;
@@ -42,42 +43,16 @@ export default function mainScreen() {
         },
       ]}
     >
-      <ScrollView
+      <FlatList
         contentContainerStyle={styles.scrollContainer}
-        showsVerticalScrollIndicator={false}
-      >
-        {/* <Deck label={decks[0].title} cardsDue={decks[0].cardsDue} />
-				<Deck
-					label={decks[1].title}
-					cardsDue={decks[1].cardsDue}
-					backgroundColor={theme.colors.pink}
-				/>
-				<Deck
-					label={decks[2].title}
-					cardsDue={decks[2].cardsDue}
-					backgroundColor={theme.colors.lightblue}
-				/>
-				<Deck
-					label={decks[3].title}
-					cardsDue={decks[3].cardsDue}
-					backgroundColor={theme.colors.green}
-				/>
-				<Deck
-					label={decks[4].title}
-					cardsDue={decks[4].cardsDue}
-					backgroundColor={theme.colors.green}
-				/>
-				<Deck
-					label={decks[0].title}
-					cardsDue={decks[0].cardsDue}
-					backgroundColor={theme.colors.green}
-				/>
-				<Deck
-					label={decks[1].title}
-					cardsDue={decks[1].cardsDue}
-					backgroundColor={theme.colors.green}
-				/> */}
-      </ScrollView>
+        data={decks}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item, index }) => {
+          const colorPalette = [theme.colors.blue, theme.colors.pink, theme.colors.lightblue, theme.colors.green, theme.colors.lightpurple]
+          return(
+          <DeckComponent label={item.name} cardsDue={0} backgroundColor={colorPalette[index % colorPalette.length]}></DeckComponent>)
+        }}
+      ></FlatList>
       <Overlay
         visible={buttonVisible}
         onPress={() => setButtonVisible(false)}
