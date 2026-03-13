666ec4f0e5813cc518ed0057755cab75ee88c730
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-13 08:00:12 +0100

    Formatting only

--- repositories/CardRepository.ts
+++ repositories/CardRepository.ts
@@ -1,16 +1,16 @@
 import { Card } from "@/models/card";
 
 export type Filters = {
-    cardId? : number,
-    deckId? : number,
-    front? : string,
-    back?: string,
-    tags? : string[],
-}
+  cardId?: number;
+  deckId?: number;
+  front?: string;
+  back?: string;
+  tags?: string[];
+};
 
 export interface CardRepository {
-    createNewCard: (cardData : Omit<Card, 'id'>) => Promise<Card>;
-    getCards: (filters? : Filters) => Promise<Card[]>;
-    updateCard: (cardData: Card) => Promise<Card>;
-    deleteCard: (cardId : number) => Promise<boolean>;
+  createNewCard: (cardData: Omit<Card, "id">) => Promise<Card>;
+  getCards: (filters?: Filters) => Promise<Card[]>;
+  updateCard: (cardData: Card) => Promise<Card>;
+  deleteCard: (cardId: number) => Promise<boolean>;
 }
