307399c74d2c9343b32085431083d7bc2bc3f7c9
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-14 08:46:02 +0100

    Added Link component as a parent for pressable and moved Pressable styles to a View component

--- components/Deck.tsx
+++ components/Deck.tsx
@@ -1,11 +1,14 @@
 import { theme } from "@/styles/theme";
-import { Pressable, StyleSheet, Text, ViewStyle } from "react-native";
+import { Link } from "expo-router";
+import { Pressable, StyleSheet, Text, View, ViewStyle } from "react-native";
 
 type Props = {
   label: string;
   cardsDue: number;
   backgroundColor?: string;
   style?: ViewStyle;
+  onPress?: () => void;
+  onLongPress?: () => void;
 };
 
 export default function DeckComponent({
@@ -13,25 +16,36 @@ export default function DeckComponent({
   cardsDue,
   backgroundColor = theme.colors.blue,
   style,
+  onPress,
+  onLongPress,
 }: Props) {
   return (
-    <Pressable style={[styles.deckContainer, { backgroundColor }, style]}>
-      <Text style={styles.deckNameText}>{label}</Text>
-      <Text style={styles.deckTextCardsDue}>{cardsDue} cards due</Text>
-    </Pressable>
+    <View style={[styles.deckContainer, { backgroundColor }, style]}>
+      <Link href={"/study"} asChild>
+        <Pressable
+          onPress={onPress}
+          onLongPress={onLongPress} style={styles.deckPressable}
+        >
+          <Text style={styles.deckNameText}>{label}</Text>
+          <Text style={styles.deckTextCardsDue}>{cardsDue} cards due</Text>
+        </Pressable>
+      </Link>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   deckContainer: {
     marginVertical: 5,
-    padding: 13,
-    width: "100%",
     flex: 1,
+    padding: 13,
     minHeight: 120,
     maxHeight: 140,
     borderRadius: theme.borderRadius.sm,
     flexDirection: "row",
+  },
+  deckPressable: {
+    width: '100%',
     justifyContent: "space-between",
   },
   deckNameText: {
