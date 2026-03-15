394a44c32bf752a0b790deb2d8fbd115445acdcc
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-15 20:32:00 +0100

    Folders structure changes - no changes in files.

--- app/study.tsx
+++ app/study.tsx
@@ -1,5 +1,7 @@
-import FlashCardContainer from "@/components/FlashCardContainter";
+import FlashCardContainer from "@/components/flashcard/FlashCardContainer";
+import StandardFront from "@/components/flashcard/front types/StandardFront";
 
 export default function study() {
-  return <FlashCardContainer></FlashCardContainer>;
+  return <FlashCardContainer>
+  </FlashCardContainer>;
 }
