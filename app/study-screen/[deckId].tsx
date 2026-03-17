a7516dca4d522eaa0f50742fddb3d90fee88bf83
Wiktoria Zietak <maullido.blog@gmail.com>
2026-03-17 07:52:55 +0100

    Added logic to increase the index of the cards being reviewed and going back to the main screen after review of the last card. Added loading screen and 'empty deck' information screen. Reviewing flashcards works on very basic level.

--- app/study-screen/[deckId].tsx
+++ app/study-screen/[deckId].tsx
@@ -1,13 +1,24 @@
+import EmptyDeckView from "@/components/EmptyDeckView";
 import FlashCardContainer from "@/components/flashcard/FlashCardContainer";
+import LoadingScreen from "@/components/LoadingScreen";
 import { Card } from "@/models/card";
 import { MockCardRepository } from "@/repositories/MockCardRepository";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 
 export default function studyScreen() {
   const [cardsForToday, setCardsForToday] = useState<Card[]>([]);
-  const [loading, setIsLoading] = useState<boolean>(true);
+  const [currentCardIndex, setCurrentCardIndex] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { deckId } = useLocalSearchParams<{ deckId: string }>();
+
+  const increaseIndex = () => {
+    if (currentCardIndex < cardsForToday.length - 1) {
+      setCurrentCardIndex(currentCardIndex + 1);
+    } else {
+      router.back();
+    }
+  };
 
   useEffect(() => {
     const prepareFlashCards = async () => {
@@ -15,7 +26,7 @@ export default function studyScreen() {
 
       try {
         const mockCards = new MockCardRepository();
-        mockCards.getCards().then((cards) => {
+        await mockCards.getCards().then((cards) => {
           const filteredCards = cards.filter(
             (card) => card.deckId === Number(deckId),
           );
@@ -30,5 +41,17 @@ export default function studyScreen() {
     prepareFlashCards();
   }, [deckId]);
 
-  return <FlashCardContainer></FlashCardContainer>;
+  if (isLoading) {
+    return <LoadingScreen></LoadingScreen>;
+  }
+  if (cardsForToday.length === 0) {
+    return <EmptyDeckView></EmptyDeckView>;
+  }
+
+  return (
+    <FlashCardContainer
+      cardData={cardsForToday[currentCardIndex]}
+      onNextCard={increaseIndex}
+    ></FlashCardContainer>
+  );
 }
