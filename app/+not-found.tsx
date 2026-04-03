78473411962a6616df4f8c7d438fd47d521a8f86
Wiktoria Zietak <maullido.blog@gmail.com>
2026-04-03 07:52:42 +0200

    Slightly changed the look of the 'Not found' screen - added potato img and changed Link text

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,35 +1,41 @@
-import { theme } from '@/styles/theme';
-import { Link, Stack } from 'expo-router';
-import { View, StyleSheet, Text } from 'react-native';
+import { theme } from "@/styles/theme";
+import { Link, Stack } from "expo-router";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function NotFoundScreen() {
-	return (
-		<>
-			<Stack.Screen options={{ title: 'Oops! Not Found' }} />
-			<View style={styles.container}>
-        <Text style={styles.text}>Oops! Not Found</Text>
-				<Link href={'/'} style={styles.button} replace>Go back to Home Screen</Link>
-			</View>
-		</>
-	);
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops! Not Found" }} />
+      <View style={styles.container}>
+        <Image source={require("@/assets/images/potato.png")} style={{height: 150, width: 150}}></Image>
+        <Text style={[styles.text, { paddingTop: 20, paddingBottom: 10 }]}>Sorry!</Text>
+        <Text style={[styles.text, { fontSize: theme.fontSize.md }]}>
+          There is nothing here but a potato.
+        </Text>
+        <Link href={"/"} style={styles.button} replace>
+          Go back
+        </Link>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.background,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    paddingBottom: 20,
+    paddingBottom: 50,
     fontSize: theme.fontSize.lg,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    textAlign: "center",
   },
   button: {
-    fontSize: theme.fontSize.md,
-    color: theme.colors.primary,
-    textDecorationLine: 'underline'
-  }
-
-})
+    fontFamily: theme.fontFamily.bold,
+    fontSize: theme.fontSize.lg,
+    color: theme.colors.purple,
+  },
+});
