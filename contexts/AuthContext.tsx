0c4b211b20eaabcc2d2508c4f52de28b3e176236
Wiktoria Zietak <maullido.blog@gmail.com>
2026-04-07 08:12:23 +0200

    Added isInitialized boolean, created new type returned for AuthContext

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -2,21 +2,30 @@ import { supabase } from "@/utils/supabase";
 import { Session } from "@supabase/supabase-js";
 import { createContext, ReactNode, useEffect, useState } from "react";
 
-export const AuthContext = createContext<null | Session>(null);
+type authContextType = {
+  currentSession: Session | null;
+  isInitialized: boolean;
+};
+
+export const AuthContext = createContext<authContextType | null>(null);
 
 export default function AuthProvider({ children }: { children: ReactNode }) {
   const [currentSession, setCurrentSession] = useState<null | Session>(null);
+  const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
     const { data } = supabase.auth.onAuthStateChange((event, session) => {
       console.log(event, session);
       setCurrentSession(session);
+      setIsInitialized(true);
     });
     return () => data.subscription.unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={currentSession}>
+    <AuthContext.Provider
+      value={{ currentSession: currentSession, isInitialized: isInitialized }}
+    >
       {children}
     </AuthContext.Provider>
   );
