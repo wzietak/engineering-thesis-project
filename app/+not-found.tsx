53683b56429b9990acae18a100c00147cfd5a5c8
Wiktoria Zietak <maullido.blog@gmail.com>
2026-04-08 07:04:45 +0200

    Replaced Link by Pressable so router could be used for better navigation experience

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,20 +1,25 @@
 import { theme } from "@/styles/theme";
-import { Link, Stack } from "expo-router";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { router, Stack } from "expo-router";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops! Not Found" }} />
       <View style={styles.container}>
-        <Image source={require("@/assets/images/potato.png")} style={{height: 150, width: 150}}></Image>
-        <Text style={[styles.text, { paddingTop: 20, paddingBottom: 10 }]}>Sorry!</Text>
+        <Image
+          source={require("@/assets/images/potato.png")}
+          style={{ height: 150, width: 150 }}
+        ></Image>
+        <Text style={[styles.text, { paddingTop: 20, paddingBottom: 10 }]}>
+          Sorry!
+        </Text>
         <Text style={[styles.text, { fontSize: theme.fontSize.md }]}>
           There is nothing here but a potato.
         </Text>
-        <Link href={"/"} style={styles.button} replace>
-          Go back
-        </Link>
+        <Pressable onPress={router.back}>
+          <Text style={styles.goBackText}>Go back</Text>
+        </Pressable>
       </View>
     </>
   );
@@ -22,6 +27,7 @@ export default function NotFoundScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 20,
     flex: 1,
     backgroundColor: theme.colors.background,
     alignItems: "center",
@@ -30,12 +36,17 @@ const styles = StyleSheet.create({
   text: {
     paddingBottom: 50,
     fontSize: theme.fontSize.lg,
-    fontWeight: "bold",
+    fontFamily: theme.fontFamily.bold,
     textAlign: "center",
   },
   button: {
     fontFamily: theme.fontFamily.bold,
     fontSize: theme.fontSize.lg,
+    color: theme.colors.purple,
+  },
+  goBackText: {
+    fontSize: theme.fontSize.md,
+    fontFamily: theme.fontFamily.regular,
     color: theme.colors.purple,
   },
 });
