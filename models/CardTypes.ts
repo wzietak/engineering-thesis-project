a51c5ba485fd476a8cbfb1ccf28cdd8671bfd3db
Wiktoria Zietak <maullido.blog@gmail.com>
2026-04-23 07:13:03 +0200

    Added new card type

--- models/CardTypes.ts
+++ models/CardTypes.ts
@@ -3,11 +3,13 @@ any typo-related bugs and make it easier to change them
 or add new types */
 enum CardType {
   BASIC = "Basic",
+  REVERSED = "Reversed",
   BASIC_AND_REVERSED = "Basic and reversed",
 }
 
 /* Mapping array for dropdown components */
 export const CARD_TYPE_OPTIONS = [
   { label: "Basic", value: CardType.BASIC },
+  { label: "Reversed", value: CardType.REVERSED },
   { label: "Basic and reversed", value: CardType.BASIC_AND_REVERSED },
 ];
