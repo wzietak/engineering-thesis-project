6bccf995c1f5298952f3ce1ab2a6d0bf08187d84
Wiktoria Zietak <maullido.blog@gmail.com>
2026-04-30 18:27:01 +0200

    changed colors for darkTheme; removed no longer used code

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -14,6 +14,7 @@ type Colors = {
   red: string;
   background_alpha: string;
   error: string;
+  icon: string;
 };
 
 type BorderRadius = {
@@ -58,41 +59,27 @@ export const lightTheme: Colors = {
   red: "#ffa0a0",
   background_alpha: "rgba(255, 255, 255, 0.2)",
   error: "red",
+  icon: "@/assets/icons/splash-icon-light-android.png",
 };
 
 export const darkTheme: Colors = {
-  background: "rgb(20, 25, 41)",
+  background: "#141929",
   primary: "rgb(112, 121, 168)",
   blue: "#5f719b",
   green: "#78936c",
   pink: "#8d718d",
   lightblue: "#6c8f8c",
-  grey: "#CFD7DA",
-  grey_light: "#dee5e8",
+  grey: "rgb(77, 84, 117)",
+  grey_light: "#677376",
   primary_light: "rgb(76, 83, 114)",
-  purple: "#7961a4",
+  purple: "#7458a9",
   purple_alpha: "#7861a43a",
   lightpurple: "#706683",
-  red: "#ffa0a0",
+  red: "#8a5858",
   background_alpha: "rgba(20, 25, 41, 0.2)",
   error: "#8d0707",
+  icon: "@/assets/icons/splash-icon-dark-android.png",
 };
-
-// export const colors = {
-//   background: "#fff",
-//   primary: "#000",
-//   blue: "#B8CBFA",
-//   green: "#D7F5CA",
-//   pink: "#F5CAF4",
-//   lightblue: "#CAF5F3",
-//   grey: "#CFD7DA",
-//   grey_light: "#dee5e8",
-//   primary_light: "#393939",
-//   purple: "#a580e6",
-//   purple_alpha: "#a580e65f",
-//   lightpurple: "#e1d0ff",
-//   red: "#ffa0a0",
-// };
 
 export const borderRadius: BorderRadius = {
   sm: 12,
@@ -133,44 +120,3 @@ export interface AppTheme {
   fontSize: FontSize;
   fontFamily: FontFamily;
 }
-
-export const theme = {
-  colors: {
-    background: "#fff",
-    primary: "#000",
-    blue: "#B8CBFA",
-    green: "#D7F5CA",
-    pink: "#F5CAF4",
-    lightblue: "#CAF5F3",
-    grey: "#CFD7DA",
-    grey_light: "#dee5e8",
-    primary_light: "#393939",
-    purple: "#a580e6",
-    purple_alpha: "#a580e65f",
-    lightpurple: "#e1d0ff",
-    red: "#ffa0a0",
-  },
-  deckColors: ["#B8CBFA", "#D7F5CA", "#F5CAF4", "#CAF5F3"],
-  borderRadius: {
-    sm: 12,
-    md: 18,
-    lg: 32,
-  },
-  boxShadow: {
-    buttons: "0px 0px 18px rgba(0, 0, 0, 0.25)",
-    small_buttons: "0px 0px 8px rgba(0, 0, 0, 0.16)",
-    bottomContainer: "0px -5px 18px rgb(255, 255, 255)",
-  },
-  fontSize: {
-    x_sm: 14,
-    sm: 18,
-    md: 20,
-    lg: 23,
-    x_lg: 26,
-  },
-  fontFamily: {
-    regular: "Rubik_400Regular",
-    bold: "Rubik_600SemiBold",
-    italic: "Rubik_400Light_Italic",
-  },
-};
