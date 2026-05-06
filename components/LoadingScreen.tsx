d4022322a5a81d945920f174afd380476a02041a
Wiktoria Zietak <maullido.blog@gmail.com>
2026-05-06 10:43:10 +0200

    fixed imports

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,9 +1,8 @@
 import { useAppTheme } from "@/contexts/ColorThemeContext";
-import { theme } from "@/styles/theme";
 import { ActivityIndicator } from "react-native";
 
 export default function LoadingScreen() {
-   const { theme } = useAppTheme();
+  const { theme } = useAppTheme();
   return (
     <ActivityIndicator
       style={{
