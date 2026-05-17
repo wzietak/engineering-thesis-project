51bdf6b57633ef8a404a3e77d061a6f3bd54155f
Wiktoria Zietak <maullido.blog@gmail.com>
2026-05-17 22:51:05 +0200

    Wrote functions calculating basic parameters fro FSRS algorithm

--- algorithm/FSRS.ts
+++ algorithm/FSRS.ts
@@ -1,1 +1,52 @@
-export class FSRS {}
+import { FSRSState } from "./FSRSState";
+import { FSRS_PARAMETERS, Grade } from "./FSRSTypes";
+
+const DESIRED_RETENTION = 0.9;
+
+export class FSRS {
+  private calculateInterval(
+    desiredRetention: number = DESIRED_RETENTION,
+    stability: number,
+  ) {
+    const interval =
+      (stability / Math.pow(0.9, -1 / FSRS_PARAMETERS[20]) - 1) *
+      (Math.pow(desiredRetention, -1 / FSRS_PARAMETERS[20]) - 1);
+
+    return interval;
+  }
+
+  private calculateInitialStability(grade: Grade): number {
+    let stability;
+    if (grade === 1) {
+      stability = FSRS_PARAMETERS[0];
+    } else if (grade === 2) {
+      stability = FSRS_PARAMETERS[1];
+    } else if (grade === 3) {
+      stability = FSRS_PARAMETERS[2];
+    } else {
+      stability = FSRS_PARAMETERS[3];
+    }
+    return stability;
+  }
+
+  private calculateShortTermStability(stability: number, grade: Grade) {
+    const e = Math.E;
+    const shortTermStability =
+      stability *
+      Math.pow(e, FSRS_PARAMETERS[17] * (grade - 3 + FSRS_PARAMETERS[18])) *
+      Math.pow(stability, -FSRS_PARAMETERS[19]);
+
+    return shortTermStability;
+  }
+
+  private calculateNewStability() {}
+
+  private calculateInitialDifficulty(grade: Grade): number {
+    const e = Math.E;
+    const initialDifficulty =
+      FSRS_PARAMETERS[4] - Math.pow(e, FSRS_PARAMETERS[5] * (grade - 1)) + 1;
+    return initialDifficulty;
+  }
+
+  public calculateCardState(FSRSState: FSRSState) {}
+}
--- algorithm/FSRSTypes.ts
+++ algorithm/FSRSTypes.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_PARAMETERS = [
+export const FSRS_PARAMETERS = [
   0.212, 1.2931, 2.3065, 8.2956, 6.4133, 0.8334, 3.0194, 0.001, 1.8722, 0.1666,
   0.796, 1.4835, 0.0614, 0.2629, 1.6483, 0.6014, 1.8729, 0.5425, 0.0912, 0.0658,
   0.1542,
@@ -11,8 +11,8 @@ export enum CardDirection {
 
 export enum State {
   New = "New",
-  Review = "Review",
   Learning = "Learning",
+  Review = "Review",
   Relearning = "Relearning",
 }
 
