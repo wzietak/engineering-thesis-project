3f692085adcb3642bc7cd8caeccaacfb6a6da02b
Wiktoria Zietak <maullido.blog@gmail.com>
2026-05-23 18:36:52 +0200

    Changed db function to get cards for Review; added different views returned depending on the situation (deck is empty or there are no flashcards to review)

--- app/study-screen/[deckId].tsx
+++ app/study-screen/[deckId].tsx
@@ -2,16 +2,21 @@ import EmptyDeckView from "@/components/EmptyDeckView";
 import FlashCardContainer from "@/components/flashcard/FlashCardContainer";
 import LoadingScreen from "@/components/LoadingScreen";
 import { AuthContext } from "@/contexts/AuthContext";
-import { Card } from "@/models/card";
-import { globalCardRepository } from "@/repositories/globalCardRepository";
+import {
+  getCardsForReview,
+  ReviewableCard,
+} from "@/repositories/flashcardReviewRepository.ts";
+import { globalDeckRepository } from "@/repositories/globalDeckRepository";
 import { router, useLocalSearchParams } from "expo-router";
 import { useContext, useEffect, useState } from "react";
 
 export default function studyScreen() {
-  const [cardsForToday, setCardsForToday] = useState<Card[]>([]);
+  // const [cardsForToday, setCardsForToday] = useState<Card[]>([]);
+  const [cardsForToday, setCardsForToday] = useState<ReviewableCard[]>([]);
   const [currentCardIndex, setCurrentCardIndex] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const { deckId } = useLocalSearchParams<{ deckId: string }>();
+  const [isDeckEmpty, setIsDeckEmpty] = useState(true);
 
   const session = useContext(AuthContext);
 
@@ -26,11 +31,15 @@ export default function studyScreen() {
   useEffect(() => {
     const prepareFlashCards = async () => {
       try {
-        await globalCardRepository.getCards(session?.currentSession?.user.id as string, deckId).then((cards) => {
-          const filteredCards = cards.filter(
-            (card) => card.deck_id === deckId,
-          );
-          setCardsForToday(filteredCards);
+        const isEmpty = await globalDeckRepository.checkIfDeckIsEmpty(deckId);
+        setIsDeckEmpty(isEmpty);
+        if (isEmpty) return;
+        getCardsForReview(
+          session?.currentSession?.user.id as string,
+          deckId,
+        ).then((cards) => {
+          const readyCards = cards as ReviewableCard[];
+          setCardsForToday(readyCards);
         });
       } catch (error) {
         console.error("Error while getting cards data.");
@@ -44,8 +53,11 @@ export default function studyScreen() {
   if (isLoading) {
     return <LoadingScreen></LoadingScreen>;
   }
-  if (cardsForToday.length === 0) {
+  if (isDeckEmpty) {
     return <EmptyDeckView></EmptyDeckView>;
+  }
+  if (cardsForToday.length === 0) {
+    return <EmptyDeckView noMoreCardsToReview={true}></EmptyDeckView>;
   }
 
   return (
