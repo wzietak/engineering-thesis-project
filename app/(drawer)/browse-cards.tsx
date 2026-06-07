28fcaf88601f334925e1abef4eece07e6f897f81
Wiktoria Zietak <maullido.blog@gmail.com>
2026-06-07 22:30:16 +0200

    Added FlatList showing all the flashcards for the user

--- app/(drawer)/browse-cards.tsx
+++ app/(drawer)/browse-cards.tsx
@@ -1,20 +1,80 @@
-import Flashcard from "@/components/Flashcard";
+import FlashcardComponent from "@/components/Flashcard";
+import { AuthContext } from "@/contexts/AuthContext";
 import { useAppTheme } from "@/contexts/ColorThemeContext";
+import { DBContext } from "@/contexts/DBContext";
+import { globalCardRepository } from "@/repositories/globalCardRepository";
 import { AppTheme } from "@/styles/theme";
+import { router, useFocusEffect } from "expo-router";
+import { useCallback, useContext, useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export type CardForBrowse = {
+  cardId: string;
+  deckId: string;
+  deckName: string;
+  cardFront: string;
+  cardBack: string;
+  nextReview: string;
+};
 
 export default function browseCards() {
+  const insets = useSafeAreaInsets();
   const { theme } = useAppTheme();
   const styles = createStyles(theme);
+  const DBconnection = useContext(DBContext);
+  const session = useContext(AuthContext);
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [cards, setCards] = useState<CardForBrowse[]>([]);
+
+  const userId = session?.currentSession?.user.id as string;
+
+  useFocusEffect(
+    useCallback(() => {
+      if (!DBconnection.isReady || !userId) {
+        return;
+      }
+      setIsLoading(true);
+      globalCardRepository
+        .getAllCardsByUser(userId)
+        .then((fetchedCards) => {
+          setCards([...(fetchedCards || [])]);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }, [DBconnection.isReady, userId]),
+  );
+
   return (
-    <View style={styles.mainContainer}>
-      <Flashcard
-        cardId=""
-        cardFront="łyżka"
-        cardBack="spoon"
-        deckName="Angielski"
-        nextReview="2026-06-07T17:42:09.000Z"
-      ></Flashcard>
+    <View style={[styles.mainContainer, { paddingBottom: insets.bottom }]}>
+      <FlatList
+        contentContainerStyle={styles.scrollContainer}
+        keyExtractor={(item) => item.cardId}
+        data={cards}
+        renderItem={({ item, index }) => {
+          return (
+            <FlashcardComponent
+              cardId={item.cardId}
+              cardFront={item.cardFront}
+              cardBack={item.cardBack}
+              deckName={item.deckName}
+              nextReview={item.nextReview}
+              onPress={() => {
+                router.push({
+                  pathname: "/add-new-card",
+                  params: { cardId: item.cardId },
+                });
+              }}
+            ></FlashcardComponent>
+          );
+        }}
+      ></FlatList>
     </View>
   );
 }
@@ -25,6 +85,9 @@ const createStyles = (theme: AppTheme) =>
       flex: 1,
       backgroundColor: theme.colors.background,
       flexDirection: "column",
+    },
+    scrollContainer: {
       paddingHorizontal: 20,
+      paddingBottom: 10,
     },
   });
