98e7028cfb9fca6da8fdd7d5cbdc02a7decb3771
Wiktoria Zietak <maullido.blog@gmail.com>
2026-09-18 16:36:22 +0200

    Changed function name - it's not ready yet but previous name was changed incorrectly; removed 'user_id' from supabase queries - after adding RLS policies it's no longer required; Added pull and push functions for FSRS states table

--- services/syncService.ts
+++ services/syncService.ts
@@ -1,3 +1,8 @@
+import {
+  getUnsyncedFSRSStates,
+  markFSRSStatesAsSynced,
+  updateUnsyncedFSRSStates,
+} from "@/repositories/flashcardReviewRepository.ts";
 import { globalCardRepository } from "@/repositories/globalCardRepository";
 import { globalDeckRepository } from "@/repositories/globalDeckRepository";
 import { supabase } from "@/utils/supabase";
@@ -5,13 +10,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const lastSync = (userId: string) => `@lastSyncTime_${userId}`;
 
-export async function getLastSyncTime(userId: string) {
+export async function syncData(userId: string) {
   try {
     const lastSyncTime = await AsyncStorage.getItem(`@lastSyncTime_${userId}`);
   } catch (error) {
     return null;
   }
-  return setLastSyncTime;
 }
 
 export async function setLastSyncTime(lastSyncTime: string, userId: string) {
@@ -20,8 +24,8 @@ export async function setLastSyncTime(lastSyncTime: string, userId: string) {
   } catch (error) {}
 }
 
-async function pullDecks(userId: string, lastSyncTime: string) {
-  let query = supabase.from("decks").select("*").eq("user_id", userId);
+async function pullDecks(userId: string, lastSyncTime: string | null) {
+  let query = supabase.from("decks").select("*"); //Added RLS policy in Supabase, so user_id is not required to select all the records belonging to the user that is signed in
 
   if (lastSyncTime) {
     query = query.gt("updated_at", lastSyncTime);
@@ -96,8 +100,8 @@ async function pushDecks(userId: string) {
   }
 }
 
-async function pullCards(userId: string, lastSyncTime: string) {
-  let query = supabase.from("cards").select("*").eq("user_id", userId);
+async function pullCards(userId: string, lastSyncTime: string | null) {
+  let query = supabase.from("cards").select("*"); //Added RLS policy in Supabase, so user_id is not required to select all the records belonging to the user that is signed in
 
   if (lastSyncTime) {
     query = query.gt("updated_at", lastSyncTime);
@@ -172,10 +176,76 @@ async function pushCards(userId: string) {
   }
 }
 
-async function pullFSRSStates() {}
+async function pullFSRSStates(userId: string, lastSyncTime: string | null) {
+  let query = supabase.from("fsrs_states").select("*");
 
-async function pullReviews() {}
+  if (lastSyncTime) {
+    query = query.gt("updated_at", lastSyncTime);
+  }
 
-async function pushFSRSStates() {}
+  const { data: serverStates, error } = await query;
+  if (error) {
+    console.log(error);
+    return;
+  }
+  if (!serverStates || serverStates.length === 0) return;
+
+  console.log(serverStates);
+  const statesMap = new Map();
+
+  const localUnsyncedStates = await getUnsyncedFSRSStates(userId);
+
+  for (const localState of localUnsyncedStates) {
+    statesMap.set(localState.id, localState);
+  }
+
+  const statesToUpsert: any[] = [];
+
+  for (const serverState of serverStates) {
+    const localState = statesMap.get(serverState.id);
+
+    if (localState) {
+      const serverStateUpdatedTimestamp = new Date(
+        serverState.updated_at,
+      ).getTime();
+      const localStateUpdatedTimestamp = new Date(
+        localState.updated_at,
+      ).getTime();
+
+      if (serverStateUpdatedTimestamp > localStateUpdatedTimestamp) {
+        statesToUpsert.push(serverState);
+      } //we don't do anything if the local date is newer - it'll be taken care of in PUSH function
+    } else {
+      statesToUpsert.push(serverState);
+    }
+  }
+
+  if (statesToUpsert.length > 0) {
+    await updateUnsyncedFSRSStates(statesToUpsert);
+  }
+}
+
+async function pushFSRSStates(userId : string) {
+   const localUnsyncedStates =
+    await getUnsyncedFSRSStates(userId);
+
+  if (localUnsyncedStates.length > 0) {
+    const { error } = await supabase.from("fsrs_states").upsert(localUnsyncedStates);
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+    await markFSRSStatesAsSynced(
+      localUnsyncedStates.map((state) => state.id),
+    );
+  }
+}
+
+async function pullReviews() {
+ 
+}
+
+
 
 async function pushReviews() {}
