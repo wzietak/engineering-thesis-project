024ab64e8786c5a7f7efcbeb7c62d01b86c05ecb
Wiktoria Zietak <maullido.blog@gmail.com>
2026-09-24 11:53:07 +0200

    Removed temporary sync button

--- components/MainOptions.tsx
+++ components/MainOptions.tsx
@@ -1,6 +1,5 @@
 import { AuthContext } from "@/contexts/AuthContext";
 import { useAppTheme } from "@/contexts/ColorThemeContext";
-import { syncData } from "@/services/syncService";
 import { AppTheme } from "@/styles/theme";
 import Octicons from "@expo/vector-icons/Octicons";
 import { useContext } from "react";
@@ -58,7 +57,7 @@ export default function MainOptions({ visible, hideOnOutline }: Props) {
               ></ToggleButton>
             </Pressable>
 
-            {/* <Pressable style={styles.menuOption}>
+            <Pressable style={styles.menuOption}>
               <Octicons
                 style={styles.menuIcon}
                 name="download"
@@ -66,18 +65,6 @@ export default function MainOptions({ visible, hideOnOutline }: Props) {
                 color="black"
               />
               <Text style={styles.menuOptionText}>Import</Text>
-            </Pressable> */}
-            <Pressable
-              style={styles.menuOption}
-              onPress={() => syncData(userId)}
-            >
-              <Octicons
-                style={styles.menuIcon}
-                name="sync"
-                size={22}
-                color="black"
-              />
-              <Text style={styles.menuOptionText}>SYNC</Text>
             </Pressable>
 
             <Pressable style={styles.menuOption}>
